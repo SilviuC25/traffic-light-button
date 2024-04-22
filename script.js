8ddf98fbd02ea7c38659e545b09919e8994f15af
VilutSilviu <vilut.chisalita@smartboxdigital.com>
2024-04-22 14:10:45 +0200

    Interval Changed v2

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let light = document.getElementById("traffic-light")
 let colorClasses = ["btn-danger", "btn-warning", "btn-success"]
 let currentColorIndex = 0
-var intervalID = window.setInterval(changeTrafficLightColor, 1e4); // change the traffic light color every 10 seconds
 
 function changeTrafficLightColor() {
     light.classList.remove(colorClasses[currentColorIndex]) // remove the existing color
     currentColorIndex = (currentColorIndex + 1) % colorClasses.length
     light.classList.add(colorClasses[currentColorIndex]) // add the next color
-    clearInterval(intervalID)
 }
+
+window.setInterval(function(){
+    changeTrafficLightColor();
+  }, 1e4); // change the traffic light color every 10 seconds
